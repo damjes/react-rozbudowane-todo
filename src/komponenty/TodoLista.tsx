8ac90e4c1eeafc5de03bcba2c16cf0d16337136f
Damjes <git@damj.es>
2025-10-29 19:08:09 +0100

    Dodano możliwość dodawania nowych zadań do listy

--- src/komponenty/TodoLista.tsx
+++ src/komponenty/TodoLista.tsx
@@ -1,11 +1,17 @@
-import type { Zadanie } from '../moduły/Zadanie'
+import { useState } from 'react'
+
+import { noweZadanie, Priorytet, type Zadanie } from '../moduły/Zadanie'
 import { generatorZmieniarek, type Zmieniarka } from '../moduły/Zmieniarka'
+
 import KomponentZadania from './KomponentZadania'
+import Ikonka from './Ikonka'
 
 function TodoLista(props: {
 	zadania: Zadanie[]
 	zmieniarkaListy: Zmieniarka<Zadanie[]>
 }) {
+	const [noweZadanieTreść, setNoweZadanieTreść] = useState('')
+
 	const zmieniarkaPojedynczegoZadania = generatorZmieniarek(
 		props.zmieniarkaListy
 	)
@@ -22,6 +28,31 @@ function TodoLista(props: {
 		<>
 			<h2 className='my-4'>Lista rzeczy do zrobienia</h2>
 			<ul className='list-group'>{listaZadań}</ul>
+			<h3 className='my-4'>Dodawanie zadania</h3>
+			<div className='input-group mb-3'>
+				<input
+					type='text'
+					className='form-control'
+					placeholder='Treść nowego zadania'
+					value={noweZadanieTreść}
+					onChange={e => setNoweZadanieTreść(e.target.value)}
+					required
+				/>
+				<button
+					className='btn btn-success'
+					type='button'
+					onClick={() => {
+						props.zmieniarkaListy(zadania => [
+							...zadania,
+							noweZadanie(noweZadanieTreść),
+						])
+						setNoweZadanieTreść('')
+					}}
+					disabled={!(noweZadanieTreść.trim().length > 0)}
+				>
+					<Ikonka nazwa='plus-lg' opis='Dodaj' />
+				</button>
+			</div>
 		</>
 	)
 }
